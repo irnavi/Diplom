c0529b7412f00c6f93d94906716cd54ef65f9c30
Irina.Nikolaev <irnavi@mail.ru>
2023-12-10 01:32:52 +0100

    create homePage

--- src/components/saleHome/SaleHome.jsx
+++ src/components/saleHome/SaleHome.jsx
@@ -18,15 +18,71 @@ function SaleHome() {
                 <div className={styles.product_card}>
                     <div className={styles.product_img}>
                         <img src="" alt="" />
+                        <div className={styles.sale_absolute}>
+                            <p className={styles.absolute_text}>-50%</p>
+                        </div>
                     </div>
-                    <div className={styles.product_title}>
-                        <p className={styles.title_text}>Decorative forged bridge</p>
+                    <div className={styles.product_info}>
+                        <div className={styles.product_title}>
+                            <p className={styles.title_text}>Decorative forged bridge</p>
+                        </div>
+                        <div className={styles.product_prise}>
+                            <p className={styles.prise_sale}>$500</p>
+                            <p className={styles.prise_default}>$1000</p>
+                        </div>
                     </div>
-                    <div className={styles.product_prise}>
-                        <p className={styles.prise_sale}>$500</p>
-                        <p className={styles.prise_default}>$1000</p>
+                </div>
+                <div className={styles.product_card}>
+                    <div className={styles.product_img}>
+                        <img src="" alt="" />
+                        <div className={styles.sale_absolute}>
+                            <p className={styles.absolute_text}>-34%</p>
+                        </div>
                     </div>
-
+                    <div className={styles.product_info}>
+                        <div className={styles.product_title}>
+                            <p className={styles.title_text}>Flower basket</p>
+                        </div>
+                        <div className={styles.product_prise}>
+                            <p className={styles.prise_sale}>$100</p>
+                            <p className={styles.prise_default}>$150</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.product_card}>
+                    <div className={styles.product_img}>
+                        <img src="" alt="" />
+                        <div className={styles.sale_absolute}>
+                            <p className={styles.absolute_text}>-25%</p>
+                        </div>
+                    </div>
+                    <div className={styles.product_info}>
+                        <div className={styles.product_title}>
+                            <p className={styles.title_text}>Aquarium lock</p>
+                        </div>
+                        <div className={styles.product_prise}>
+                            <p className={styles.prise_sale}>$150</p>
+                            <p className={styles.prise_default}>$200</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.product_card}>
+                    <div className={styles.product_img}>
+                        <img src="" alt="" />
+                        <div className={styles.sale_absolute}>
+                            <p className={styles.absolute_text}>-17%</p>
+                        </div>
+                    </div>
+                    <div className={styles.product_info}>
+                        <div className={styles.product_title}>
+                            <p className={styles.title_text}>Secateurs</p>
+                        </div>
+                        <div className={styles.product_prise}>
+                            <p className={styles.prise_sale}>$199</p>
+                            <p className={styles.prise_default}>$240</p>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
