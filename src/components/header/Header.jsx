fa26f6610174a8b1cef14f90b045b1c66b2ccc38
Irina.Nikolaev <irnavi@mail.ru>
2024-02-11 19:19:30 +0100

    added navlink and adaptiv from button addToCart

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 import styles from "./Header.module.css"
 import logo from "../../assets/image/logo.svg"
 import ic_cart from "../../assets/image/cart_icon.svg"
@@ -12,32 +12,66 @@ function Header() {
 
     let count = calculateCartCount(list)
 
+    const linkActive = ({ isActive}) => isActive ? styles.nav_link : styles.link_default
+
 
     
-    return(
-        <header>
-            <div className={styles.container}>
-                <div className={styles.header_items}> 
-                    <div className={styles.header_logo}>
-                    <Link to="/"><img src={logo} alt="logo_from_gard" /></Link>
-                    </div>
-                    <div className={styles.header_navigation}>
-                        <ul>
-                            <li><Link to="/" className={styles.nav_link}>Main Page</Link></li>
-                            <li><Link to="/categories" className={styles.nav_link}>Categories</Link></li>
-                            <li><Link to="/products" className={styles.nav_link}>All products</Link></li>
-                            <li><Link to="/sales" className={styles.nav_link}>All sales</Link></li>
-                        </ul>
-                    </div>
-                    <div className={styles.header_cart}>
-                    <Link to="/cart"><img src={ic_cart} alt="cart" /></Link>
-                    <div className={count === 0 ? styles.count_none : styles.count_cart}>{count}</div>
-                    </div>
-                </div>
+    return (
+      <header>
+        <div className={styles.container}>
+          <div className={styles.header_items}>
+            <div className={styles.header_logo}>
+              <Link to="/">
+                <img src={logo} alt="logo_from_gard" />
+              </Link>
             </div>
+            <div className={styles.header_navigation}>
 
-        </header>
-    )
+              <ul>
+                <li>
+                  <NavLink
+                    to="/" className={linkActive}>
+                    Main Page
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/categories"
+                    className={linkActive}>
+                    Categories
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/products"
+                    className={linkActive}>
+                    All products
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/sales" 
+                  className={linkActive}>
+                    All sales
+                  </NavLink>
+                </li>
+              </ul>
+
+
+            </div>
+            <div className={styles.header_cart}>
+              <Link to="/cart">
+                <img src={ic_cart} alt="cart" />
+              </Link>
+              <div
+                className={count === 0 ? styles.count_none : styles.count_cart}
+              >
+                {count}
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
 }
 
 export default Header;
